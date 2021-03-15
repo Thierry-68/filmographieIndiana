cf77ef9166ff19db954af667a6430797bae900ec
Thierry-68 <thierry.sandre68@gmail.com>
2021-03-15 22:40:43 +0100

    add index.html && update readme.md

--- index.php
+++ index.php
@@ -1,11 +1,36 @@
-# Filmographie Indiana
-**Crée la filmofgraphie d'indiana**
+<?php
+/* 
+    Créer la filmographie d'Indiana
 
     Dans un fichier PHP, crée un tableau contenant 3 titres de films dans lesquels joue notre ami Indiana.
+
     Pour chaque film, associe son année de sortie (le titre du film sera la clé).
+
     Une fois le tableau créé, réalise une boucle pour afficher la liste des films ainsi que leur année de sortie associée.
+
     Les films devront apparaître dans l'ordre croissant de sortie (regarde du côté des fonctions de tri).
     Pour chaque film, tu devras afficher:
 
     2000 - movie_title
     2000 étant l'année de parution du film. 
+*/
+
+    $titres=["Les Aventuriers de l'arche perdu","Indiana Jones et la Dernière Croisade","Indiana Jones 5"];
+
+    $releaseYear[$titres[0]]=1981;
+    $releaseYear[$titres[1]]=1989;
+    $releaseYear[$titres[2]]=2022;
+
+    echo "<ul>";
+    foreach ($titres as $key => $value)
+    {
+        echo "<li>$releaseYear[$value] - $value</li> ";
+    }
+    echo "</ul>";
+
+
+
+
+
+
+
